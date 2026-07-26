0e5f954d0c98c951072eba11957060aced3dfc6e
Emylie <emylieperiot@gmail.com>
2026-07-26 15:13:10 +0100

    starting work on projects and recent work section, creating files

---
+++ data/projects.ts
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    title: "Bespoke Storage",
+    category: "Carpentry",
+    image: "/images/projects/bespoke-storage.jpg",
+  },
+  {
+    title: "Garden Refresh",
+    category: "Outdoor Maintenance",
+    image: "/images/projects/garden-refresh.jpg",
+  },
+  {
+    title: "Property Repairs",
+    category: "Property Maintenance",
+    image: "/images/projects/property-repairs.jpg",
+  },
+];
+
+export default projects;
